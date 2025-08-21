6b5557481f74826d13ba8a1a03404056a2a49194
Waseem Sattar <analysts.develop@gmail.com>
2025-08-21 03:03:58 -0700

    addConfiguration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   // output: 'export', // 👈 static export mode
-  };
-  
-  export default nextConfig;
+   output: 'export', // 👈 static export mode
+  images: {
+    unoptimized: true, // 🚀 disable optimization globally
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ don’t block builds on lint errors
+  },
+};
+
+export default nextConfig;
