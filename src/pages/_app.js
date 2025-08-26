08b73f92ef0b8aa8a3be2bfea07db21e9495756a
Waseem Sattar <analysts.develop@gmail.com>
2025-08-26 08:57:28 -0700

    addButtons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import { Provider } from "react-redux";
 import store from "../redux/store/store";
 import AOS from "aos";
@@ -9,8 +10,20 @@ import "aos/dist/aos.css";
 // import "../styles/globals.css"; // Uncomment if needed
 import '../index.scss';
 import GlobalMeta from "../components/GlobalMeta";
+import WhatsAppButton from "../components/WhatsAppButton";
+import CallButton from "../components/CallButton";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+
+  // define paths where you don’t want buttons
+  const hiddenPaths = ["/login", "/blog-create", "/blog-list"];
+  const shouldHideButtons = hiddenPaths.some((path) =>
+    router.pathname.startsWith(path)
+  );
+
+
   useEffect(() => {
     AOS.init({
       offset: 0,
@@ -24,6 +37,14 @@ function MyApp({ Component, pageProps }) {
     <Provider store={store}>
       <GlobalMeta />
       <Component {...pageProps} />
+       {/* ✅ Conditionally render buttons like in React Router */}
+       {!shouldHideButtons && (
+        <>
+
+          <WhatsAppButton />
+          <CallButton />
+        </>
+      )}
     </Provider>
   );
 }
